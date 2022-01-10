e1b5af7962d1cf705ec9049e1532ad675c41ddde
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-01-10 10:30:38 +0530

    models added

---
+++ src/main/java/com/springboot/bankapp/model/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.springboot.bankapp.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id; 
+}
